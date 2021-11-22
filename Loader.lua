74b0cfaf34cb4be5fd7dfc993d8c4d6be3e8a791
WindyKung <86519657+WindyKung@users.noreply.github.com>
2021-11-22 19:32:56 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,10 @@
 local _gamelists = {
-    CB_Loder = 301549746, --Counter Blox
-    AS_Loder = 286090429, --Arsenal
-    BF_Loder = 2753915549, --BloxFruit
-    SB_Loder = 734159876, --SharkBite
-    PF_Loder = 292439477, --Phantom Forece
-    WLS_Loder = 6792241488 --Weight Lifting Simulator
+    CB_Loader = 301549746, --Counter Blox
+    AS_Loader = 286090429, --Arsenal
+    BF_Loader = 2753915549, --BloxFruit
+    SB_Loader = 734159876, --SharkBite
+    PF_Loader = 292439477, --Phantom Forece
+    WLS_Loader = 6792241488 --Weight Lifting Simulator
 }
 
 for i,v in pairs(_gamelists) do
