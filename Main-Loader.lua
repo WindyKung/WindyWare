a65ed98e8f1906c4818030c670e276fa1abae55c
WindyKung <86519657+WindyKung@users.noreply.github.com>
2021-12-13 11:53:54 +0700

    Update Main-Loader.lua

--- Main-Loader.lua
+++ Main-Loader.lua
@@ -1,11 +1,12 @@
 --[[
 local _gamelists = {
-    CB_Loader = 301549746, --Counter Blox
-    AS_Loader = 286090429, --Arsenal
-    BF_Loader = 2753915549, --BloxFruit
-    SB_Loader = 734159876, --SharkBite
-    PF_Loader = 292439477, --Phantom Forece
-    WLS_Loader = 6792241488 --Weight Lifting Simulator
+    CB_Loader  = 301549746,  --Counter Blox
+    AS_Loader  = 286090429,  --Arsenal
+    BF_Loader  = 2753915549, --BloxFruit
+    SB_Loader  = 734159876,  --SharkBite
+    PF_Loader  = 292439477,  --Phantom Forece
+    WLS_Loader = 6792241488, --Weight Lifting Simulator
+    DS_Loader  = 4410049285  --Driving Simulator
 }
 
 for i,v in pairs(_gamelists) do
@@ -26,5 +27,9 @@ if game.PlaceId == 286090429 then --Arsenal
 end
 
 if game.PlaceId == 292439477 then -- Phantom Force
+    loadstring(game:HttpGet(""))()
+end
+
+if game.PlaceId == 4410049285 then -- Driving Simulator
     loadstring(game:HttpGet(""))()
 end
