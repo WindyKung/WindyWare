d8c0082978aa2723411a27ef46ed99a8178fb04d
WindyKung <86519657+WindyKung@users.noreply.github.com>
2022-05-22 21:33:33 +0700

    Update DrivingEmpire.lua

--- MainScript/DrivingEmpire.lua
+++ MainScript/DrivingEmpire.lua
@@ -1,5 +1,5 @@
-	game:GetService("StarterGui"):SetCore("SendNotification", {
-		Title    = "WindyWare";
-		Text     = "Script Is Down Temporary, Please Wait";
-		Duration = 5
-	})
+game:GetService("StarterGui"):SetCore("SendNotification", {
+	Title    = "WindyWare";
+	Text     = "Script Is Down Temporary, Please Wait\nYou Can Use V3.1 While Script Is Updating <3";
+	Duration = 5
+})
